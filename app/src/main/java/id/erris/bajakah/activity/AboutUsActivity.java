afbdfdfdbd7c1fc16a1864cac57ad6f6a07be779
erawan <erawan@infinite.web.id>
2021-10-12 04:00:23 +0700

    perbaikan about us

--- app/src/main/java/id/erris/bajakah/activity/AboutUsActivity.java
+++ app/src/main/java/id/erris/bajakah/activity/AboutUsActivity.java
@@ -43,10 +43,38 @@ public class AboutUsActivity extends AppCompatActivity {
         binding.imgAboutUsImageId.setOnClickListener(v -> {
             binding.lblAboutUsTitle.setText(getResources().getText(R.string.about_us_title_id));
             binding.lblAboutUsContent.setText(getResources().getText(R.string.about_us_content_id));
+
+            binding.lblAboutLogoTitle.setText(getResources().getText(R.string.about_logo_title_id));
+            binding.lblAboutLogoContent.setText(getResources().getText(R.string.about_logo_content_id));
+
+            binding.lblAboutSloganSandy.setText(getResources().getText(R.string.duta_bahasa_kalimantan_tengah_2021));
+            binding.lblAboutSloganWinney.setText(getResources().getText(R.string.duta_bahasa_kalimantan_tengah_2021));
+
+            binding.lblAboutNamaSandy.setText(getResources().getText(R.string.nama_panggilan_sandy_id));
+            binding.lblAboutTtlSandy.setText(getResources().getText(R.string.tempat_tanggal_lahir_sandy_id));
+            binding.lblAboutEmailSandy.setText(getResources().getText(R.string.pos_el_sandy_id));
+
+            binding.lblAboutNamaWinney.setText(getResources().getText(R.string.nama_panggilan_winney_id));
+            binding.lblAboutTtlWinney.setText(getResources().getText(R.string.tempat_tanggal_lahir_winney_id));
+            binding.lblAboutEmailWinney.setText(getResources().getText(R.string.pos_el_winney_id));
         });
         binding.imgAboutUsImageEn.setOnClickListener(v -> {
             binding.lblAboutUsTitle.setText(getResources().getText(R.string.about_us_title_en));
             binding.lblAboutUsContent.setText(getResources().getText(R.string.about_us_content_en));
+
+            binding.lblAboutLogoTitle.setText(getResources().getText(R.string.about_logo_title_en));
+            binding.lblAboutLogoContent.setText(getResources().getText(R.string.about_logo_content_en));
+
+            binding.lblAboutSloganSandy.setText(getResources().getText(R.string.central_kalimantan_language_ambassador_2021));
+            binding.lblAboutSloganWinney.setText(getResources().getText(R.string.central_kalimantan_language_ambassador_2021));
+
+            binding.lblAboutNamaSandy.setText(getResources().getText(R.string.nama_panggilan_sandy_en));
+            binding.lblAboutTtlSandy.setText(getResources().getText(R.string.tempat_tanggal_lahir_sandy_en));
+            binding.lblAboutEmailSandy.setText(getResources().getText(R.string.pos_el_sandy_en));
+
+            binding.lblAboutNamaWinney.setText(getResources().getText(R.string.nama_panggilan_winney_en));
+            binding.lblAboutTtlWinney.setText(getResources().getText(R.string.tempat_tanggal_lahir_winney_en));
+            binding.lblAboutEmailWinney.setText(getResources().getText(R.string.pos_el_winney_en));
         });
     }
 }
